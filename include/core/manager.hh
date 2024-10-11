5917f45fef3bdb5586fc38c94d41921e7721d19f
Edward Wang <edawordwang@gmail.com>
2024-10-11 18:32:12 -0400

    cleanup manager

--- include/core/manager.hh
+++ include/core/manager.hh
@@ -15,46 +15,29 @@ public:
         Track,
         PluginSelector,
     };
+
     Manager(te::Edit &edit);
     void SetCurrentTrack(size_t track_manager);
-
     void Render(Interface& interface);
     void HandleEvent(const Event& event);
-
     void AddTrack();
 
-    // TODO use array instead
     te::Edit &edit_ ;
-    const std::vector<Color> colors_ = {
-        Color{0x29, 0x73, 0x55, 0xff}, 
-        Color{0x1e, 0x4d, 0x3f, 0xff},
-        Color{0xdd, 0xe6, 0x63, 0xff},
-        Color{0x7d, 0x54, 0x39, 0xff},
-    };
     ScreenState screen_state_;
 
     size_t current_track_;
     std::vector<std::unique_ptr<Track>> tracks_;
     juce::Array<te::AudioTrack*> base_tracks_;
 
-    std::function<void(const std::string &)> plugin_sel_callback_ = [this] (const std::string &name) {
-        screen_state_ = ScreenState::Timeline;
-        std::cout << name << std::endl;
-        
-        std::unique_ptr<Plugin> p;
-        auto base = edit_.getPluginCache().createNewPlugin(name.c_str(), {}).get();
-
-        // TODO use cast?
-        if (name == te::ChorusPlugin::xmlTypeName) {
-            p = std::make_unique<Chorus>(base);
-        } else if (name == te::FourOscPlugin::xmlTypeName) {
-            p = std::make_unique<FourOsc>(base);
-        }
-
-        tracks_[current_track_]-> AddPlugin(std::move(p));
-    };
     PluginSelector plugin_sel_;
     Timeline timeline_;
+     
+    const std::vector<Color> colors_ = {
+        Color{0x29, 0x73, 0x55, 0xff}, 
+        Color{0x1e, 0x4d, 0x3f, 0xff},
+        Color{0xdd, 0xe6, 0x63, 0xff},
+        Color{0x7d, 0x54, 0x39, 0xff},
+    };
 };
 
 } // namespace box
