eb8a9d1cbef2a7d349866eebb759429163b8140b
Edward Wang <edawordwang@gmail.com>
2024-10-16 18:37:25 -0400

    FourOsc: formatting

--- src/plugin/plugin_four_osc.cc
+++ src/plugin/plugin_four_osc.cc
@@ -11,19 +11,25 @@ FourOsc:: FourOsc(te::Plugin *p):
     UnloadImage(four);
 
     // TODO for testing effects
-    for (int i = 0; i < base_plugin_->oscParams.size(); i++) {
+    for (int i = 0; i < base_plugin_->oscParams.size(); i++)
+    {
         base_plugin_->oscParams[i]->waveShapeValue.setValue(2, nullptr);
     }
 }
 
-FourOsc:: ~FourOsc() {
+FourOsc:: ~FourOsc()
+{
     UnloadTexture(icon_);
 }
 
-void FourOsc:: Render(Interface &interface) {
-    for (int y = 0; y < Interface::HEIGHT; ++y) {
-        for (int x = 0; x < Interface::WIDTH; ++x) {
-            if (x % 2 == 0 && y % 2 == 0) {
+void FourOsc:: Render(Interface &interface)
+{
+    for (int y = 0; y < Interface::HEIGHT; ++y)
+    {
+        for (int x = 0; x < Interface::WIDTH; ++x)
+        {
+            if (x % 2 == 0 && y % 2 == 0)
+            {
                 DrawCircle(x, y, 3, RED);
             }
         }
@@ -31,24 +37,28 @@ void FourOsc:: Render(Interface &interface) {
     knob_master_level_.Render(interface);
 }
 
-void FourOsc:: HandleEvent(const Event &event) {
-    switch (event.type) {
-        case EventType::KeyPress:
-            switch(event.value) {
-                case KEY_DOWN:
-                    LOG_VAR(knob_master_level_.param_.GetNorm()); knob_master_level_.param_.SetNorm(knob_master_level_.param_.GetNorm() - 0.125); break;
-                case KEY_UP:
-                    LOG_VAR(knob_master_level_.param_.GetNorm());
-                    knob_master_level_.param_.SetNorm(knob_master_level_.param_.GetNorm() + 0.125);
-                    break;
-            }
+void FourOsc:: HandleEvent(const Event &event)
+{
+    switch (event.type)
+    {
+    case EventType::KeyPress:
+        switch(event.value)
+        {
+        case KEY_DOWN:
+            LOG_VAR(knob_master_level_.param_.GetNorm()); knob_master_level_.param_.SetNorm(knob_master_level_.param_.GetNorm() - 0.125); break;
+        case KEY_UP:
+            LOG_VAR(knob_master_level_.param_.GetNorm());
+            knob_master_level_.param_.SetNorm(knob_master_level_.param_.GetNorm() + 0.125);
             break;
-        case EventType::KeyRelease:
-            break;
+        }
+        break;
+    case EventType::KeyRelease:
+        break;
     }
 }
 
-Texture2D FourOsc:: GetIcon() const {
+Texture2D FourOsc:: GetIcon() const
+{
     return icon_;
 }
 
