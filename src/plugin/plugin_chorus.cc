930deff5d9fdb1aaaca43a44e13ca54b0761355e
Edward Wang <edawordwang@gmail.com>
2024-10-17 15:10:25 -0400

    Chorus: use new Param

--- src/plugin/plugin_chorus.cc
+++ src/plugin/plugin_chorus.cc
@@ -5,8 +5,13 @@ namespace box {
 
 Chorus::Chorus(te::Plugin *p)
     : Plugin(p), base_plugin_{static_cast<te::ChorusPlugin *>(p)},
-    knob_depth_{CV<float>{base_plugin_->depthMs, 0.0, 10.0}}
-    // knob_depth_{32, 64, 5, {0, 255, 0}, base_plugin_->depthMs, nullptr}
+    knob_depth_{
+        Parameter<float>{CV<float>{base_plugin_->depthMs, 0.0, 10.0}},
+        32, // x
+        32, // y
+        16, // radius
+        GREEN
+    }
 {
     Image icon = LoadImage("assets/star_16x16.png");
     icon_ = LoadTextureFromImage(icon);
@@ -22,13 +27,12 @@ void Chorus::HandleEvent(const Event &event) {
         case EventType::KeyPress:
             switch(event.value) {
                 case KEY_ONE:
-                    // LOG_VAR(knob_depth_.param_.GetValue());
-                    LOG_VAR(knob_depth_.cv_.GetNorm());
-                    knob_depth_.cv_.SetNorm(knob_depth_.cv_.GetNorm() + 0.125);
+                    LOG_VAR(knob_depth_.param_.GetNorm());
+                    knob_depth_.param_.SetNorm(knob_depth_.param_.GetNorm() + 0.125);
                     break;
                 case KEY_TWO:
-                    LOG_VAR(knob_depth_.cv_.GetNorm());
-                    knob_depth_.cv_.SetNorm(knob_depth_.cv_.GetNorm() - 0.125);
+                    LOG_VAR(knob_depth_.param_.GetNorm());
+                    knob_depth_.param_.SetNorm(knob_depth_.param_.GetNorm() - 0.125);
                     break;
             }
             break;
