47d282a54da284ce6e8ef5442e88b153b6677b1e
Edward Wang <edawordwang@gmail.com>
2024-10-17 15:10:17 -0400

    Knob: use new Param

--- include/component/knob.hh
+++ include/component/knob.hh
@@ -6,22 +6,14 @@
 namespace box {
 
 template <typename T>
-class Knob {
-public:
-
-    // TODO add min, max, name
-    // Knob(uint8_t x, uint8_t y, uint8_t radius, Color color, juce::CachedValue<T> &value, te::AutomatableParameter *ap = nullptr, std::string name = ""):
-    //     x_{x}, y_{y}, radius_{radius}, color_{color}, param_{value, ap}, name_{name}
-    // {}
-
-    Knob(CV<T> cv)
-    : cv_{cv}, x_{32}, y_{32}, radius_{16}, color_{GREEN}, name_{"yoo"}
+struct Knob {
+    Knob(Parameter<T> p, uint8_t x, uint8_t y, uint8_t radius, Color color, std::string name = "")
+    : param_{p}, x_{x}, y_{y}, radius_{radius}, color_{color}, name_{name}
     {}
 
     virtual void Render(Interface &interface) 
     {
-        // auto angle = param_.GetNorm() * 360;
-        auto angle = cv_.GetNorm() * 360;
+        auto angle = param_.GetNorm() * 360;
         auto x = static_cast<float>(x_);
         auto y = static_cast<float>(y_);
         DrawCircleSector(Vector2{x, y}, radius_, 0.f, angle, 360, GREEN);
@@ -31,9 +23,7 @@ public:
         
     }
 
-    CV<T> cv_;
-    // Parameter<T> param_;
-private:
+    Parameter<T> param_;
     uint8_t x_, y_, radius_;
     Color color_;
     std::string name_;
