9ed27bafd73350453453da106e560e4886d4a364
Edward Wang <edawordwang@gmail.com>
2024-10-17 14:04:16 -0400

    use CV in knob instead of Parameter

--- include/component/knob.hh
+++ include/component/knob.hh
@@ -1,6 +1,7 @@
 #pragma once
 #include "core/util.hh"
 #include "core/parameter.hh"
+#include "core/cv.hh"
 
 namespace box {
 
@@ -9,13 +10,18 @@ class Knob {
 public:
 
     // TODO add min, max, name
-    Knob(uint8_t x, uint8_t y, uint8_t radius, Color color, juce::CachedValue<T> &value, te::AutomatableParameter *ap = nullptr, std::string name = ""):
-        x_{x}, y_{y}, radius_{radius}, color_{color}, param_{value, ap}, name_{name}
+    // Knob(uint8_t x, uint8_t y, uint8_t radius, Color color, juce::CachedValue<T> &value, te::AutomatableParameter *ap = nullptr, std::string name = ""):
+    //     x_{x}, y_{y}, radius_{radius}, color_{color}, param_{value, ap}, name_{name}
+    // {}
+
+    Knob(CV<T> cv)
+    : cv_{cv}, x_{32}, y_{32}, radius_{16}, color_{GREEN}, name_{"yoo"}
     {}
 
     virtual void Render(Interface &interface) 
     {
-        auto angle = param_.GetNorm() * 360;
+        // auto angle = param_.GetNorm() * 360;
+        auto angle = cv_.GetNorm() * 360;
         auto x = static_cast<float>(x_);
         auto y = static_cast<float>(y_);
         DrawCircleSector(Vector2{x, y}, radius_, 0.f, angle, 360, GREEN);
@@ -25,7 +31,8 @@ public:
         
     }
 
-    Parameter<T> param_;
+    CV<T> cv_;
+    // Parameter<T> param_;
 private:
     uint8_t x_, y_, radius_;
     Color color_;
