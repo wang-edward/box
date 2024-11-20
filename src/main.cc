cf0bb9e7d9504787bb6716f70c10b666c8b53ffa
Edward Wang <edawordwang@gmail.com>
2024-11-19 21:44:23 -0500

    wip

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,6 @@
 #include <iostream>
 #include <filesystem>
 
-#include <juce_events/juce_events.h>
-
 #include "core/util.hh"
 #include "core/interface.hh"
 #include "core/track.hh"
---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+Test(ExampleTest, BasicAssertions) {
+    EXPECT_EQ(1 + 1, 2);
+}
