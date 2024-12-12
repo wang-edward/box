3470f9209e8769df30b10ed0c64a7e6f077fa87f
edward wang <edwardfeiwang@gmail.com>
2024-12-12 01:29:02 -0500

    pass through x11 in script

--- docker.sh
+++ docker.sh
@@ -4,14 +4,31 @@
 IMAGE_NAME="box"
 CONTAINER_NAME="container"
 
-# Build the Docker image
-docker build -t $IMAGE_NAME .
+# Ensure .asoundrc exists on the host
+if [ ! -f ~/.asoundrc ]; then
+    echo 'pcm.!default { type plug slave.pcm "hw:0,0" }' > ~/.asoundrc
+fi
 
-# Run the Docker container with ALSA support
+# Allow X11 connections
+xhost +local:docker
+
+## Check if the Docker image exists
+if ! docker image inspect $IMAGE_NAME >/dev/null 2>&1; then
+    echo "Docker image '$IMAGE_NAME' not found. Building the image..."
+    docker build -t $IMAGE_NAME .
+else
+    echo "Docker image '$IMAGE_NAME' already exists. Skipping build."
+fi
+
+# Run the Docker container with X11 and ALSA support
 docker run --rm -it \
-  --name $CONTAINER_NAME \
-  --device /dev/snd \
-  -v /usr/share/alsa:/usr/share/alsa \
-  -v /etc/asound.conf:/etc/asound.conf \
-  -v ~/.asoundrc:/root/.asoundrc \
-  $IMAGE_NAME
+    --name $CONTAINER_NAME \
+    --device /dev/snd \
+    -v /usr/share/alsa:/usr/share/alsa \
+    -v /etc/asound.conf:/etc/asound.conf \
+    -v ~/.asoundrc:/root/.asoundrc \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e XDG_RUNTIME_DIR=/tmp/xdg_runtime_dir \
+    --tmpfs /tmp/xdg_runtime_dir:rw \
+    $IMAGE_NAME
