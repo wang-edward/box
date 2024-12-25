9a985b2e3c8ed86ff7ea5366ef100ab77a321b64
Richard Sha <113735719+richardsha19@users.noreply.github.com>
2024-12-24 22:58:44 -0500

    Implemented Switch.hh struct (#131)
    * Implemented Switch.hh struct
    
    * Made Improvements to switch.hh
    
    * make constructor api match knob
    
    * typo?
    
    ---------
    
    Co-authored-by: Edward Wang <edawordwang@gmail.com>

---
+++ include/component/switch.hh
@@ -0,0 +1,35 @@
+#pragma once
+#include "core/util.hh"
+#include "core/parameter.hh"
+
+// instead of using cv.hh, we directly use juce::CachedValue<bool>
+
+namespace box
+{
+
+struct Switch
+{
+    juce::CachedValue<bool> &value_;
+    uint8_t x_, y_;
+    Color color_;
+    std::string name_;
+
+    Switch(juce::CachedValue<bool> &value, uint8_t x, uint8_t y, Color color, std::string name)
+    : value_{value}, x_{x}, y_{y}, color_{color}, name_{name}
+    {}
+
+    virtual void Render(Interface &interface) 
+    {
+        Color text_color = value_ ? color_ : WHITE;
+        const int font_size = 10;
+        int width = MeasureText(name_.c_str(), font_size);
+        DrawText(name_.c_str(), x - (width / 2), y + 20, font_size, text_color);
+    }
+
+    virtual void HandleEvent(const Event& event)
+    {
+
+    }
+};
+
+} // namespace box
