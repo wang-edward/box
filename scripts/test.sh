62cf7aba5ab92d199673e44b321df1e3fddf47c9
Edward Wang <edawordwang@gmail.com>
2025-01-20 18:01:43 -0500

    runscripts

---
+++ scripts/test.sh
@@ -0,0 +1,18 @@
+
+BIN_PATH="./build/BoxTests"
+
+cmake -DCMAKE_BUILD_TYPE=Debug -B build
+
+if [[ $? == 0 ]]; then
+    make -C build -j6
+else
+    echo "cmake failed"
+    exit 1
+fi
+
+if [[ $? == 0 ]]; then
+    $BIN_PATH
+else
+    echo "make failed"
+    exit 1
+fi
