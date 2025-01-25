e45eecde407f0246786ac996e69c8d9cafe9f353
Edward Wang <edawordwang@gmail.com>
2025-01-25 01:51:51 -0500

    copy paste

--- test/unit_test.cc
+++ test/unit_test.cc
@@ -15,3 +15,63 @@ TEST(ExampleTest, BasicAssertions)
     EXPECT_EQ(1 + 1, 2);
     EXPECT_TRUE(true);
 }
+
+TEST(ALSA, PrintMidiIn)
+{
+    const juce::ScopedJuceInitialiser_GUI initialiser; // need this
+
+    te::Engine engine{"Tracktion Hello World"};
+    std::filesystem::path curr_path = std::filesystem::current_path();
+    juce::File my_file{juce::String{curr_path.string() + "/tmp.box"}};
+
+    std::unique_ptr<te::Edit> my_edit = createEmptyEdit(engine, my_file);
+    te::Edit &edit = *my_edit;
+    edit.ensureNumberOfAudioTracks(8);
+    edit.getTransport().ensureContextAllocated();
+    box::Interface interface {
+    };
+
+    box::App app(engine, edit);
+    box::APP = &app;
+
+    // test ALSA::PrintMidiIn
+    {
+        auto vec = engine.getDeviceManager().getMidiInDevices();
+        std::cout << "midiInputs.size(): " << vec.size() << std::endl;
+        for (auto &x : vec)
+        {
+            std::cout << x->getName() << std::endl;
+        }
+    }
+}
+
+// this generates different code than main()
+// where the assert(ptr != nullptr) passes in main(),
+//   ASSERT_NE(ptr, nullptr) fails.
+TEST(ALSA, VirtualMidi)
+{
+    const juce::ScopedJuceInitialiser_GUI initialiser; // need this
+    SetTargetFPS(60);
+
+    te::Engine engine{"Tracktion Hello World"};
+    std::filesystem::path curr_path = std::filesystem::current_path();
+    juce::File my_file{juce::String{curr_path.string() + "/tmp.box"}};
+
+    std::unique_ptr<te::Edit> my_edit = createEmptyEdit(engine, my_file);
+    te::Edit &edit = *my_edit;
+    edit.ensureNumberOfAudioTracks(8);
+    edit.getTransport().ensureContextAllocated();
+    box::Interface interface {
+    };
+
+    box::App app(engine, edit);
+    box::APP = &app;
+
+    // test ALSA::VirtualMidi
+    {
+        engine.getDeviceManager().createVirtualMidiDevice("box_midi");
+        engine.getDeviceManager().setDefaultMidiInDevice("box_midi");
+        auto ptr = engine.getDeviceManager().getDefaultMidiInDevice();
+        ASSERT_NE(ptr, nullptr);
+    }
+}
